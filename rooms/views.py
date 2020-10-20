fa742e0145c33b8042d445882610ed177f3dcf02
gnshjoo <gnshjoo@gmail.com>
2020-10-20 20:19:51 +0900

    class veiw define

--- rooms/views.py
+++ rooms/views.py
@@ -1,23 +1,19 @@
-from math import ceil
-from django.shortcuts import render
+from django.views.generic import ListView
 from . import models
 
 
-def all_rooms(request):
-    page = request.GET.get("page", 1)
-    page = int(page or 1)
-    page_size = 10
-    limit = page_size * page
-    offset = limit - page_size
-    all_rooms = models.Room.objects.all()[offset:limit]
-    page_count = models.Room.objects.count() / page_size
-    return render(
-        request,
-        "rooms/home.html",
-        {
-            "rooms": all_rooms,
-            "page": page,
-            "count_page": ceil(page_count),
-            "page_range": range(1, ceil(page_count))
-        }
-     )
+class HomeView(ListView):
+
+    """ HomeView Definition"""
+
+    model = models.Room
+    paginate_by = 10
+    ordering = "created"
+    paginate_orphans = 5
+    # page_kwarg = "potato"
+    context_object_name = "rooms"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["now"] = "hello"
+        return context
