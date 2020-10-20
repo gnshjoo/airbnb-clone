74df1a978a6565ecbddce2270e49d9d1d719de34
gnshjoo <gnshjoo@gmail.com>
2020-10-20 18:50:12 +0900

    make manual page

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,23 @@
+from math import ceil
 from django.shortcuts import render
 from . import models
 
 
 def all_rooms(request):
-    all_rooms = models.Room.objects.all()
-    return render(request, "rooms/home.html", context={"rooms": all_rooms})
+    page = request.GET.get("page", 1)
+    page = int(page or 1)
+    page_size = 10
+    limit = page_size * page
+    offset = limit - page_size
+    all_rooms = models.Room.objects.all()[offset:limit]
+    page_count = models.Room.objects.count() / page_size
+    return render(
+        request,
+        "rooms/home.html",
+        {
+            "rooms": all_rooms,
+            "page": page,
+            "count_page": ceil(page_count),
+            "page_range": range(1, ceil(page_count))
+        }
+     )
