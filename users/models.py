c4d062262c68cee5941a7bad4451453ee1e025bc
gnshjoo <gnshjoo@gmail.com>
2020-12-04 15:06:19 +0900

    #13.6

--- users/models.py
+++ users/models.py
@@ -29,6 +29,6 @@ class User(AbstractUser):
     gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
     bio = models.TextField(blank=True)
     birthdate = models.DateField(blank=True, null=True)
-    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, blank=True)
+    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=3, blank=True)
     currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, blank=True)
     superhost = models.BooleanField(default=False)
