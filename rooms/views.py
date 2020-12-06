28ef9da49ab944326be1c6e53423e28ee5737d6c
gnshjoo <gnshjoo@gmail.com>
2020-12-06 21:01:19 +0900

    #13.10 paginator create

--- rooms/views.py
+++ rooms/views.py
@@ -84,7 +84,17 @@ class SearchView(View):
 
                 rooms = models.Room.objects.filter(**filter_args)
 
+                qs = models.Room.objects.filter(**filter_args).order_by("-created")
+
+                paginator = Paginator(qs, 10, orphans=5)
+
+                page = request.GET.get("page", 1)
+
+                rooms = paginator.get_page(page)
+        
+            return render(request, "rooms/search.html", {"form": form, "rooms": rooms})
+
         else:
             form = forms.SearchForm()
-
-        return render(request, "rooms/search.html", {"form": form, "rooms": rooms})
+            return render(request, "rooms/search.html", {"form": form})
+        
