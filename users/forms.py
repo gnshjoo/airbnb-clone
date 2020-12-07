23a6cc76575c18ce0b569695c8ca2f25c84d047b
gnshjoo <gnshjoo@gmail.com>
2020-12-07 14:50:34 +0900

    #15 Create Signup And Save Database

--- users/forms.py
+++ users/forms.py
@@ -18,3 +18,30 @@ class LoginForm(forms.Form):
                 self.add_error("password", forms.ValidationError("Passsword is wrong"))
         except models.User.DoesNotExist:
             self.add_error("email", forms.ValidationError("User does not exist"))
+
+class SignUpForm(forms.ModelForm):
+
+    class Meta:
+        model = models.User
+        fields = ("first_name", "last_name", "email")
+
+    password = forms.CharField(widget=forms.PasswordInput)
+    password1 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+
+    def clean_password1(self):
+        password = self.cleaned_data.get("password")
+        password1 = self.cleaned_data.get("password1")
+
+        if password != password1:
+            raise froms.ValidationError("Password confirmation does not match")
+        else:
+            return password
+
+    def save(self, *args, **kargs):
+        user = super().save(commit=False)
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+        user.username = email
+        user.set_password(password)
+        user.save()
+        
