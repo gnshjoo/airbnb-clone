cda0927faeedba938c92b48fec8992e8b371098b
gnshjoo <gnshjoo@gmail.com>
2020-12-07 12:35:10 +0900

    #14.4 Hard Way from Login

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth import authenticate, login, logout
 from . import forms
 
 class LoginView(View):
@@ -10,6 +11,18 @@ class LoginView(View):
 
     def post(selt, request):
         form = forms.LoginForm(request.POST)
-        print(form)
+        if form.is_valid():
+            email = form.cleaned_data.get("email")
+            password = form.cleaned_data.get("password")
+            user = authenticate(request, username=email, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect(reverse("core:home"))
+
+        return render(request, "users/login.html", {"form": form})
 
 
+def log_out(request):
+
+    logout(request)
+    return redirect(reverse("users:login"))
