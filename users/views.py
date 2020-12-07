c69fd4a891756037431e2817dab002e2aa014fe2
gnshjoo <gnshjoo@gmail.com>
2020-12-07 11:27:02 +0900

    #14.1 Login View, Forms created

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
+from django.views import View
 from django.shortcuts import render
+from . import forms
 
-# Create your views here.
+class LoginView(View):
+
+    def get(self, request):
+        form = forms.LoginForm()
+        return render(request, "users/login.html", {"form": form})
+
+    def post(selt, request):
+        form = forms.LoginForm(request.POST)
+        print(form)
+
+
