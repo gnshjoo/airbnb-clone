77d8c93e5bcfd38446240ce5468a6cbe37fa41fc
gnshjoo <gnshjoo@gmail.com>
2020-12-07 12:45:59 +0900

    #14 Login with FormView

--- users/views.py
+++ users/views.py
@@ -1,25 +1,23 @@
 from django.views import View
+from django.urls import reverse_lazy
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import authenticate, login, logout
+from django.views.generic import FormView
 from . import forms
 
-class LoginView(View):
+class LoginView(FormView):
 
-    def get(self, request):
-        form = forms.LoginForm()
-        return render(request, "users/login.html", {"form": form})
+    template_name= "users/login.html"
+    form_class = forms.LoginForm
+    success_url = reverse_lazy("core:home")
 
-    def post(selt, request):
-        form = forms.LoginForm(request.POST)
-        if form.is_valid():
-            email = form.cleaned_data.get("email")
-            password = form.cleaned_data.get("password")
-            user = authenticate(request, username=email, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect(reverse("core:home"))
-
-        return render(request, "users/login.html", {"form": form})
+    def form_valid(self, form):
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
 
 
 def log_out(request):
